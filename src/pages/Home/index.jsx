bbda669947cd3a63e515349e96fb3fe3f88e5808
Uítalo <uitalo.urss@gmail.com>
2022-04-17 00:27:56 -0300

    estilização do input e do botão

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export function Home() {
+
+  return (
+    <div className='container'>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome." />
+      <button>Adicionar</button>
+    </div>
+  )
+}
